a2824366cc8e6498c3ec40f5e679f266ce234b79
Edgar <lefevreedg@gmail.com>
2020-08-20 11:02:01 +0200

    test hook

--- conda_check_env.sh
+++ conda_check_env.sh
@@ -2,6 +2,9 @@
 
 echo "The hook is running"
 FILE=env.yaml
+errCode=0
 if ! [[ -f "$FILE" ]]; then
     echo "$FILE does not exist, run conda env export > env.yaml to create it !"
+    errCode=1
 fi
+exit $errCode
