8a9e59fd73ef780b791dc6e779b3d38c112d8f45
Edgar <lefevreedg@gmail.com>
2020-08-20 14:12:34 +0200

    add conda check env

--- conda_check_env.sh
+++ conda_check_env.sh
@@ -4,6 +4,9 @@ FILE=env.yaml
 errCode=0
 if ! [[ -f "$FILE" ]]; then
     errCode=1
+    conda activate CI
+    conda env export > env.yaml
+    conda deactivate
     echo "$FILE does not exist, run conda env export > env.yaml to create it !"
 fi
 exit $errCode
