96cc69a5e3eef4bc96df4b298d97b9345bc4094b
Edgar <lefevreedg@gmail.com>
2020-08-24 10:29:23 +0200

    check deps get envname clean

--- conda_check_deps.sh
+++ conda_check_deps.sh
@@ -2,37 +2,51 @@
 
 
 filename='environment.yml'
+
 n=-1 #blank line at the end
 deps=0
 errCode=0
+first_line=0
 
-while read line; do
-    # reading each line
-    var=$line
-    if [[ $deps == 1 ]];then
-    	n=$((n+1))
-    fi   
-    if [[ "$var" == "dependencies:" ]];then
-    	deps=1
+if [[ -f $filename ]]; then
+
+    while read line; do
+        # reading each line
+        var=$line
+        
+        if [[ $first_line == 0 ]]; then
+            IFS=' ' read -ra line_array <<< "$var"
+            ENV_NAME= $line_array[1]
+            echo $ENV_NAME
+
+        if [[ $deps == 1 ]];then
+        	n=$((n+1))
+        fi   
+        
+        if [[ "$var" == "dependencies:" ]];then
+        	deps=1
+        fi
+
+    done < $filename
+
+
+    # folder
+
+    if [[ -z "${CONDA_EXE}" ]]; then
+      conda_var="${_CONDA_EXE}"
+    else
+      conda_var="${CONDA_EXE}"
     fi
 
-done < $filename
+    conda_path="$(dirname $(dirname "$conda_var"))"/envs/"$ENV_NAME"/conda-meta/""
+    n_folder=$(ls $conda_path | wc -l)
 
-
-# folder
-
-if [[ -z "${CONDA_EXE}" ]]; then
-  conda_var="${_CONDA_EXE}"
+    if ! [[ $n == $n_folder ]];then
+    	errCode=1
+    	echo "environment.yml is not up to date"
+    fi
 else
-  conda_var="${CONDA_EXE}"
+    errCode=1
+    echo "environment.yml does not exist"
 fi
-
-conda_path="$(dirname $(dirname "$conda_var"))"/envs/"$(basename $PWD)"/conda-meta/""
-n_folder=$(ls $conda_path | wc -l)
-
-if ! [[ $n == $n_folder ]];then
-	errCode=1
-	echo "environment.yml is not up to date"
-fi
-
 exit $errCode
