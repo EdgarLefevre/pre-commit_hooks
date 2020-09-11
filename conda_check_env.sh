17d1fb51c6cb5d6cb608bc1c926b7d6a378d8df4
Edgar <lefevreedg@gmail.com>
2020-09-11 15:36:25 +0200

    change check conda env message

--- conda_check_env.sh
+++ conda_check_env.sh
@@ -4,6 +4,6 @@ FILE=environment.yml
 errCode=0
 if ! [[ -f "$FILE" ]]; then
     errCode=1
-    echo "$FILE does not exist, run conda env export > environment.yml to create it !"
+    echo "$FILE does not exist, to create it, run: conda env export > environment.yml"
 fi
 exit $errCode
